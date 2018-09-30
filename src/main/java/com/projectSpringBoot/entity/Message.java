a1f936025ec16ea400e877fbc65e8f74e73dde71
Montego <bar_d91@mail.ru>
2018-09-30 15:18:39 +0300

    Initial commit

---
+++ src/main/java/com/projectSpringBoot/entity/Message.java
@@ -0,0 +1,34 @@
+package com.projectSpringBoot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+
+    private String tag;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message(String text, String tag, User user) {
+        this.text = text;
+        this.tag = tag;
+        this.author = user;
+    }
+
+    public String getAuthorName(){
+        return author!=null ? author.getUsername(): " <none> ";
+    }
+    public Message() {
+    }
+}
---
+++ src/main/java/com/projectSpringBoot/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.projectSpringBoot.repository;
+
+import com.projectSpringBoot.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
