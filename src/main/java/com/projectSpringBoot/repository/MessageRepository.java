8fc1468a996bfbfe6a72fe82aeb674852ae03890
Montego <bar_d91@mail.ru>
2018-10-13 17:24:44 +0300

    add pagination

--- src/main/java/com/projectSpringBoot/repository/MessageRepository.java
+++ src/main/java/com/projectSpringBoot/repository/MessageRepository.java
@@ -1,10 +1,13 @@
 package com.projectSpringBoot.repository;
 
 import com.projectSpringBoot.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByTag(String tag);
+    Page<Message> findAll(Pageable pageable);
+    Page<Message> findByTag(String tag, Pageable pageable);
+
 }
