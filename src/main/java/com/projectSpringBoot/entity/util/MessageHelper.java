620d748483b324c6a2ae0725d7b32a02d914ccf8
Montego <bar_d91@mail.ru>
2018-10-18 22:19:29 +0300

    little fix 2.0

--- src/main/java/com/projectSpringBoot/entity/util/MessageHelper.java
+++ src/main/java/com/projectSpringBoot/entity/util/MessageHelper.java
@@ -3,6 +3,7 @@ package com.projectSpringBoot.entity.util;
 import com.projectSpringBoot.entity.User;
 
 public abstract class MessageHelper {
+    //add
     public static String getAuthorName(User author){
         return author != null ? author.getUsername() : " <none> ";
     }
